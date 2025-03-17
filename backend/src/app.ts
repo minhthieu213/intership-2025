0d6fcad4b1aefe0ca1597aa1914a04fe9bd17955
kien78 <tkienn286@gmail.com>
2025-03-17 16:59:54 +0700

    Updatebe

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,5 +2,5 @@ import express from "express";
 import columnRoutes from "./routes/columnRoutes";
 
 const app = express();
-app.use(express.json()); // Important: Make sure this is added
+app.use(express.json()); 
 app.use("/columns", columnRoutes);
